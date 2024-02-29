7973c18291e1d7097d864e5c8c9f84521aaa253f
melak-12 <melakabebeee@gmail.com>
2024-02-29 13:20:23 +0300

    rm report.md

---
+++ src/app/chat/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./chatt.css";
+const ChattRoom = () => {
+  return (
+    <div className='chatt text-white p-20'>
+      <div className=' p-60 backdrop-blur-xl ring-4 ring-slate-600 rounded-lg '>
+        chatt
+      </div>{" "}
+    </div>
+  );
+};
+
+export default ChattRoom;
