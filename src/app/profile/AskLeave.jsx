f37ba1a3b805a0151cbc33e2ba9bf626979ceeda
melak-12 <melakabebeee@gmail.com>
2024-03-18 00:21:02 +0300

    profile updated  with responsive

--- src/app/profile/AskLeave.jsx
+++ src/app/profile/AskLeave.jsx
@@ -13,16 +13,16 @@ const AskLeave = () => {
     setClearance(true);
   };
   return (
-    <>
-      <div className='w-1/3  md:h-[calc(82vh-0rem)] mb-11 -mt-4 mx-8 rounded-2xl  shadow-2xl ring-1  ring-slate-200 '>
-        <div className='flex flex-col  mb-9 '>
-          <span className='text-xl p-7 text-blue-600'>Personal</span>
-          <div className='flex p-9 gap-5 justify-center flex-col'>
+    <div className='w-full  flex flex-col mx-2 md:flex-row'>
+      <div className='md:w-1/3  md:h-[calc(82vh-0rem)] mb-3 md:mb-11 -mt-4 md:mx-8 rounded-2xl  shadow-sm md:shadow-2xl ring-1  ring-slate-200 '>
+        <div className='flex flex-col  mb-2 md:mb-9 '>
+          <span className='md:text-xl p-3 md:p-7 text-blue-600'>Personal</span>
+          <div className='flex p-2 md:p-9 gap-2 md:gap-5 justify-center md:flex-col'>
             {leave && (
               <input
                 name=''
                 // onChange={"onChange"}
-                className='shadow h-8 bg-slate-100 appearance-none ring-1 ring-slate-400 border rounded-lg w-full py-2 px-3  text-slate-700 leading-tight focus:outline-none focus:shadow-outline'
+                className='shadow hidden md:block h-6 md:h-8 bg-slate-100 appearance-none ring-1 ring-slate-400 border rounded-lg w-full py-2 px-3  text-slate-700 leading-tight focus:outline-none focus:shadow-outline'
                 id='header text'
                 type='text'
                 placeholder='new'
@@ -30,7 +30,7 @@ const AskLeave = () => {
             )}
             <span
               onClick={leaveTab}
-              className={`p-1 px-8 text-lg cursor-pointer rounded-md ${
+              className={`p-1 md:px-8 text-sm md:text-lg cursor-pointer rounded-md ${
                 leave ? "bg-slate-200" : "bg-slate-3200"
               } py-3 text-slate-400`}
             >
@@ -38,7 +38,7 @@ const AskLeave = () => {
             </span>
             <span
               onClick={clearanceTab}
-              className={`p-1 px-8 text-lg cursor-pointer rounded-md ${
+              className={`p-1 md:px-8 text-sm md:text-lg cursor-pointer rounded-md ${
                 clearance ? "bg-slate-200" : "bg-slate-3200"
               } py-3 text-slate-400`}
             >
@@ -47,17 +47,22 @@ const AskLeave = () => {
           </div>
         </div>
       </div>
-      <div className='w-3/4 shadow-2xl mx-8 rounded-2xl -mt-4 mb-11 ring-1 flex-wrap  ring-slate-200  '>
+      <div className='md:w-3/4 shadow-2xl mx-2 md:mx-8 rounded-2xl md:-mt-4 mb-11 ring-1 flex-wrap  ring-slate-200  '>
         <>
           {leave && (
             <div className='flex flex-wrap  flex-row     mb-9 '>
               <div className='flex flex-col  mb-9 '>
-                <span className='text-xl p-7 font-bold text-blue-600'>
+                <span className='md:text-xl p-2 md:p-7 font-bold text-blue-600'>
                   Leave
                 </span>
-                <div className='flex p-9 gap-5 justify-center flex-row '>
-                  <div className='flex flex-row gap-14 justify-around'>
-                    <label htmlFor=''>type</label>
+                <div className='flex p-3 md:p-9 gap-2 md:gap-5 justify-center flex-col md:flex-row  '>
+                  <div className='flex flex-row gap-4 md:gap-14 justify-around'>
+                    <label
+                      htmlFor=''
+                      className='text-xs font-bold md:text-normal text-slate-500'
+                    >
+                      type
+                    </label>
                     <input
                       name=''
                       // onChange={"onChange"}
@@ -68,7 +73,12 @@ const AskLeave = () => {
                     />{" "}
                   </div>
                   <div className='flex flex-row gap-7'>
-                    <label htmlFor=''>type</label>
+                    <label
+                      htmlFor=''
+                      className='text-xs font-bold md:text-normal text-slate-500'
+                    >
+                      type
+                    </label>
                     <input
                       name=''
                       // onChange={"onChange"}
@@ -80,9 +90,14 @@ const AskLeave = () => {
                   </div>
                 </div>
                 <form action=''>
-                  <div className='flex p-9 gap-5 justify-center flex-row '>
-                    <div className='flex flex-row gap-14'>
-                      <label htmlFor=''>type</label>
+                  <div className='flex p-2 md:p-9 gap-2 md:gap-5 justify-center flex-row '>
+                    <div className='flex flex-row gap-2 md:gap-14'>
+                      <label
+                        htmlFor=''
+                        className='text-xs font-bold md:text-normal text-slate-500'
+                      >
+                        type
+                      </label>
                       <input
                         name=''
                         // onChange={"onChange"}
@@ -92,8 +107,13 @@ const AskLeave = () => {
                         placeholder='stick leave'
                       />{" "}
                     </div>
-                    <div className='flex flex-row gap-7'>
-                      <label htmlFor=''>type</label>
+                    <div className='flex flex-row gap-2 md:gap-7'>
+                      <label
+                        htmlFor=''
+                        className='text-xs font-bold md:text-normal text-slate-500'
+                      >
+                        type
+                      </label>
                       <input
                         name=''
                         // onChange={"onChange"}
@@ -104,19 +124,24 @@ const AskLeave = () => {
                       />{" "}
                     </div>
                   </div>
-                  <div className='flex flex-row gap-14 p-9'>
-                    <label htmlFor=''>remark</label>
+                  <div className='flex flex-row gap-5 md:gap-14 p-5 md:p-9'>
+                    <label
+                      htmlFor=''
+                      className='text-xs font-bold md:text-normal text-slate-500'
+                    >
+                      remark
+                    </label>
                     <textarea
                       name=''
                       // onChange={"onChange"}
-                      className='shadow h-64 bg-slate-100 appearance-none ring-1 ring-slate-400 border rounded-lg w-full py-2 px-3  text-slate-700 leading-tight focus:outline-none focus:shadow-outline'
+                      className='shadow py-7 md:h-64 bg-slate-100 appearance-none ring-1 ring-slate-400 border rounded-lg w-full md:py-2 px-3  text-slate-700 leading-tight focus:outline-none focus:shadow-outline'
                       id='description'
                       type='text'
                       placeholder='section description'
                     />
                   </div>
 
-                  <div className='flex justify-center p-9 w-96 '>
+                  <div className='flex justify-center  md:p-9 md:w-96 '>
                     <input
                       name=''
                       // onChange={"onChange"}
@@ -131,19 +156,19 @@ const AskLeave = () => {
             </div>
           )}
           {clearance && (
-            <div className='flex flex-wrap  flex-row     mb-9 '>
+            <div className='flex flex-wrap  flex-row w-full    mb-9 '>
               <div className='flex flex-col  mb-9  w-full'>
-                <span className='text-xl p-7 font-bold text-blue-600'>
+                <span className='text-xl p-3 md:p-7 font-bold text-blue-600'>
                   Clearance
                 </span>
-                <div className='flex p-9 gap-5 justify-center  flex-wrap flex-row '>
+                {/* <div className='flex p-9 gap-5 justify-center  flex-wrap flex-row '>
                   <div className='flex flex-row gap-14 w-full text-lg font-bold justify-around'>
                     <span htmlFor=''>unit</span>
                     <span>name</span>
                     <span>confirmation</span>
                     <span>date</span>
                   </div>
-      
+
                   <div className='flex flex-row flex-wrap gap-4 w-full overflow-y-scroll h-5/6 justify-around'>
                     {[1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 19].map((item) => (
                       <div
@@ -180,13 +205,68 @@ const AskLeave = () => {
                       done
                     </span>
                   </div>
-                </div>
+                </div> */}
               </div>
+              <div className='table-container w-32 md:w-full mx-3 md:mx-10 overflow-scroll  ring-1 ring-slate-200 shadow-sm bg-white rounded-lg h-[calc(40vh)] md:h-[calc(60vh)]'>
+                <table>
+                  <tbody>
+                    <tr className='text-lg  md:w-full md:font-bold'>
+                      <th>unit</th>
+                      <th>name</th>
+                      <th>confirmation</th>
+                      <th>date</th>
+                    </tr>
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 19].map((item) => (
+                      <tr
+                        key={item}
+                        className='text-slate-500 flex-row md:gap-14 w-full justify-around'
+                      >
+                        <td className='w-24 px-7 md:w-full py-3'>
+                          {item}.general store
+                        </td>
+                        <td className='w-24 px-7 md:w-full py-3'>john doe</td>
+                        <td className='w-24 px-7 md:w-full py-3'>
+                          {item === 4 ||
+                          item === 3 ||
+                          item === 6 ||
+                          item === 11 ||
+                          item === 15 ? (
+                            <button
+                              data-modal-hide='default-modal'
+                              type='button'
+                              className={`text-white bg-red-300 rounded-full focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-1`}
+                            >
+                              rejected
+                            </button>
+                          ) : (
+                            <button
+                              data-modal-hide='default-modal'
+                              type='button'
+                              className={`text-white bg-blue-300 rounded-full focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-1`}
+                            >
+                              accepted
+                            </button>
+                          )}
+                        </td>
+                        <td className='w-24 px-7 md:w-full py-3'>12-24-202</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <span className='flex justify-end'>
+                <span
+                  colSpan='4'
+                  className='flex justify-end text-blue-600  font-bold px-5 py-1'
+                >
+                  done
+                </span>
+              </span>
             </div>
           )}
         </>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -84,11 +84,11 @@ const ProfileDashboard = () => {
     <div className='mt-4 '>
       <div className=''>
         <div className='mt-16 flex justify-center  '>
-          <div className='   mx-8 rounded-2xl shadow-xl ring-1 w-1/3 ring-slate-200'>
+          <div className='  mx-2  md:mx-8 rounded-2xl md:shadow-xl ring-1  md:w-1/4 ring-slate-200'>
             <div className='flex flex-col gap-5 w-full'>
               <span className='bg-white rounded-lg '>
                 <span className='text-2xl flex justify-center  -mt-12 text-center'>
-                  <Link href='/profile'>
+                  <Link href='/profile' className='w-14 md:w-auto'>
                     <Image
                       src='/elsabet.jpeg'
                       alt=''
@@ -109,10 +109,10 @@ const ProfileDashboard = () => {
                         <Popover.Panel
                           className={`${
                             open ? "block" : "hidden"
-                          } absolute z-10 bg-white border border-gray-200 right-0 p-5 rounded-md`}
+                          } absolute z-10 bg-white border border-gray-200 right-0 p-3 md:p-5 rounded-md`}
                         >
                           <>
-                            <div className='flex justify-center flex-col w-40 text-sm gap-4 text-slate-600 '>
+                            <div className='flex justify-center flex-col text-xs md:w-40 md:text-sm gap-4 text-slate-600 '>
                               <span
                                 className='cursor-pointer '
                                 onClick={() => setStatus("leave")}
@@ -136,25 +136,25 @@ const ProfileDashboard = () => {
 
                   {/* </Popover> */}
                 </span>
-                <span className='text-2xl flex justify-center'>
+                <span className='text-xs my-2  font-bold md:text-2xl md:mx-2 flex justify-center'>
                   Misrak Berhanu
                 </span>
                 <span className='text-xs text-slate-700 flex justify-center'>
                   TF 100000329
                 </span>
-                <span className='bg-slate-200  font-thin text-sm p-2 px-3 rounded-2xl text-slate-600 mx-11 my-2 flex justify-center text-center'>
+                <span className='bg-slate-200  font-thin text-sm p-2 px-3 rounded-2xl text-slate-600  md:mx-11 my-2 flex justify-center text-center'>
                   Trainer
                 </span>
-                <span className='text-slate-800  font-thin text-sm p-2 px-3 flex justify-center mb-11 text-center'>
+                <span className='text-slate-800  font-thin text-sm p-2 px-3 flex justify-center md:mb-11 text-center'>
                   IT department
                 </span>
               </span>
-              <div className='text-slate-600 font-md bg-slate-200 h-full text-sm p-2 px-9 flex flex-col justify-center  text-center'>
+              <div className='text-slate-600 font-md bg-slate-200 h-full text-sm  md:p-2 gap-2 md:px-9 flex flex-col justify-center  text-center'>
                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17].map(
                   (item) => (
                     <div
                       key={item}
-                      className='flex  text-lg justify-start gap-4 pb-1 items-center  flex-row'
+                      className='flex  text-xs md:text-lg justify-start gap-1 md:gap-4 pb-1 items-center  flex-row'
                     >
                       <span>
                         <i className='fa fa-save'></i>
@@ -168,15 +168,18 @@ const ProfileDashboard = () => {
           </div>
           {/* <div className='w-4/5 bg-slate-200'> */}
           {status === "" ? (
-            <>
-              <div className='w-2/3  mx-8 rounded-2xl  '>
-                <div className='flex flex-col flex-wrap   shadow-lg rounded-xl ring-1 mb-9 ring-slate-200'>
-                  <span className='text-xl p-7'>data</span>
+            <div className='w-full flex flex-col md:flex-row'>
+              <div className='md:w-2/3 mx-2 md:mx-8 rounded-2xl  '>
+                <div className='flex flex-col flex-wrap my-2  md:shadow-lg rounded-xl ring-1 md:mb-9 ring-slate-200'>
+                  <span className='text-xl p-3 md:p-7'>data</span>
                   <div className='flex  flex-wrap '>
                     {[22, 16, 134, 6, 4].map((item) => (
-                      <div key={item} className='flex text-slate-500 p-4  '>
-                        <div className='flex flex-col'>
-                          <span className='text-2xl'>{item}</span>
+                      <div
+                        key={item}
+                        className='flex text-slate-500 p-2 md:p-4  '
+                      >
+                        <div className='flex text-xs flex-col'>
+                          <span className='md:text-2xl'>{item}</span>
                           <span>work days this month</span>
                         </div>
                       </div>
@@ -188,9 +191,9 @@ const ProfileDashboard = () => {
                     </span>
                   </div>
                 </div>
-                <div className='flex flex-wrap h-2/3 rounded-xl shadow-lg ring-1  ring-slate-200'>
-                  <div className='flex mt-9 w-full h-56 flex-wrap flex-row justify-center gap-20'>
-                    <div className=' rounded-md ring-1 ring-slate-200 p-9 shadow-md'>
+                <div className='flex flex-wrap md:h-2/3 rounded-xl md:shadow-lg ring-1  ring-slate-200'>
+                  <div className='flex md:mt-9 w-full md:h-56 flex-wrap flex-row justify-center gap-2 md:gap-20'>
+                    <div className=' rounded-md md:ring-1 ring-slate-200  p-3 md:p-9 shadow-md'>
                       <span className='text-1xl pb-3 mb-4'>issues</span>
                       {[1, 2, 3, 4].map((item) => (
                         <span
@@ -204,7 +207,7 @@ const ProfileDashboard = () => {
                         </span>
                       ))}
                     </div>
-                    <div className=' rounded-md ring-1 ring-slate-200 p-9  shadow-md'>
+                    <div className=' rounded-md md:ring-1 ring-slate-200  p-3 md:p-9  shadow-md'>
                       <span className='text-1xl '>accomplishments</span>
                       {[1, 2, 3].map((item) => (
                         <span
@@ -223,15 +226,15 @@ const ProfileDashboard = () => {
                       ))}
                     </div>
                   </div>
-                  <div className='flex w-full justify-end p-9'>
+                  <div className='flex w-full justify-end p-3 md:p-9'>
                     <span className='p-1  h-6  px-4 text-xs rounded-xl bg-blue-600 text-blue-200'>
                       log
                     </span>
                   </div>
-                  <div className='flex w-full justify-center gap-11 p-0'>
+                  <div className='flex mx-2 flex-col md:flex-row md:w-full justify-center gap-1 md:gap-11 p-0'>
                     <span
-                      className={` p-1 px-3 w-52 text-center text-lg h-52 rounded-xl bg-blue-400
-              text-blue-200`}
+                      className={` p-1 px-3 md:w-52 text-center text-lg md:h-52 rounded-xl bg-blue-400
+                       text-blue-200`}
                     >
                       performance
                       <Sparkline
@@ -254,7 +257,7 @@ const ProfileDashboard = () => {
                       />
                     </span>
                     <span
-                      className={`p-1 px-3 w-52 text-center text-lg h-52 rounded-xl bg-fuchsia-500 text-blue-200`}
+                      className={`p-1 px-3 md:w-52 text-center text-lg h-52 rounded-xl bg-fuchsia-500 text-blue-200`}
                     >
                       time management
                       <Sparkline
@@ -275,31 +278,20 @@ const ProfileDashboard = () => {
                         fillOpacity={0.6}
                         strokeWidth={2}
                       />
-                      {/* <AreaChart
-                        h={300}
-                        data={data}
-                        dataKey='date'
-                        series={[
-                          { name: "Apples", color: "indigo.6" },
-                          { name: "Oranges", color: "blue.6" },
-                          { name: "Tomatoes", color: "teal.6" },
-                        ]}
-                        curveType='linear'
-                      /> */}
                     </span>
                   </div>
                 </div>
               </div>
-              <div className='w-2/3 h-64  mx-8 rounded-2xl   '>
-                <div className='flex flex-wrap h-full justify-center text-xl  w-full  mb-9 '>
+              <div className='md:w-2/3 md:h-64  mx-2 md:mx-8 rounded-2xl   '>
+                <div className='flex mt-2 flex-wrap md:h-full justify-center md:text-xl  w-full  md:mb-9 '>
                   <div className='flex  flex-wrap '>
                     <div
-                      className='w-ful '
+                      className='w-full '
                       style={{ border: "  ", borderRadius: 4 }}
                     >
-                      <span className='flex gap-3 text-slate-500 h-64 w-full'>
+                      <span className='flex gap-3 text-slate-500 md:h-64 w-full'>
                         <div
-                          className='w-96 h-96'
+                          className='md:w-96 md:h-96'
                           style={{ border: "  ", borderRadius: 4 }}
                         >
                           <Calendar
@@ -313,23 +305,27 @@ const ProfileDashboard = () => {
                   </div>
                 </div>
 
-                <div className='flex flex-wrap h-72 w-full mt-32 mb-9 ring-slate-200'>
+                <div className='flex ring-1 md:ring-0 rounded-xl flex-wrap md:h-72 w-full mt-3 md:mt-32 md:mb-9 ring-slate-200'>
                   <div className='flex flex-row justify-between w-full'>
-                    <span className='text-xl p-7'>Todays Appointment</span>
-                    <span className='text-xl p-7 text-blue-600'>see all</span>
+                    <span className='md:text-xl p-3 md:p-7'>
+                      Todays Appointment
+                    </span>
+                    <span className='md:text-xl p-3 md:p-7 text-blue-600'>
+                      see all
+                    </span>
                   </div>
-                  <div className='flex flex-col w-full gap-5 p-6  flex-wrap '>
+                  <div className='flex flex-col w-full gap-2 md:gap-5 p-2 md:p-6  flex-wrap '>
                     {[1, 2, 3, 4].map((appointment) => (
                       <div key={appointment} className='flex '>
                         <span
-                          className={`p-1 px-3 w-full text-center text-xs shadow-lg h-20 border-l-8 ${
+                          className={`p-1 px-3 w-full text-center text-xs shadow-lg md:h-20 border-l-8 ${
                             appointment === 2
                               ? "border-green-300"
                               : " border-yellow-300"
                           } rounded-xl text-slate-400`}
                         >
                           <div className='flex flex-row justify-between'>
-                            <span className='flex flex-col gap-4 '>
+                            <span className='flex flex-col gap-2 md:gap-4 '>
                               <span className='text-lg text-black'>
                                 meeting
                               </span>
@@ -338,7 +334,7 @@ const ProfileDashboard = () => {
                             <span className='flex flex-row gap-4  pt-5'>
                               <span className='pt-2'>4:00 pm</span>
                               <span
-                                className={`rounded-full h-9 w-9 ${
+                                className={`rounded-full h-5 md:h-9 w-5 md:w-9 ${
                                   appointment === 1 && "bg-red-300"
                                 } bg-slate-300`}
                               ></span>
@@ -350,7 +346,7 @@ const ProfileDashboard = () => {
                   </div>
                 </div>
               </div>
-            </>
+            </div>
           ) : status === "leave" ? (
             <AskLeave />
           ) : (
